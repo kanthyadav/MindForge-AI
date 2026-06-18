1df389c6bab610fb7596ca6eff0118577a393cf2
kantyadav1817@gmail.com <kantyadav1817@gmail.com>
2026-06-18 20:01:24 +0530

    Create register controller

---
+++ backend/controllers/authController.js
@@ -0,0 +1,60 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const register = async (req, res) => {
+  try {
+    const {
+      name,
+      email,
+      password,
+    } = req.body;
+
+    const existingUser =
+      await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "User already exists",
+      });
+    }
+
+    const hashedPassword =
+      await bcrypt.hash(
+        password,
+        10
+      );
+
+    const user =
+      await User.create({
+        name,
+        email,
+        password:
+          hashedPassword,
+      });
+
+    res.status(201).json({
+      success: true,
+      message:
+        "User registered successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  register,
+};
